46e552abad5820ae258f133c4b2f5e455f03e178
initializedd <bertzhallows@gmail.com>
2023-04-10 04:01:27 +0100

    Added function to automatically set clips from a sprite

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,12 +1,49 @@
 #include "Particle.h"
 
 Particle::Particle()
-	: m_sprite{}
+	: m_texture{}
 	, m_clips{}
 {
 }
 
 Particle::~Particle()
 {
-	m_sprite.free();
+}
+
+Texture& Particle::getTexture()
+{
+	return m_texture;
+}
+
+SDL_Rect* Particle::getClips()
+{
+	return m_clips;
+}
+
+/*
+ * Calculates the coordinates of each texture in a sprite and then assigns
+ * each coordinate to its corresponding element in an SDL_Rect array.
+ *
+ * @note This function assumes the sprite is aligning each texture using a
+ * binary tree algorithm for size efficiency.
+ *
+ * @param[in] width of textures within sprite
+ * @param[in] height of textures within sprite
+ * @param[in] padding of textures within sprite
+ */
+void Particle::setClipsFromSprite(int width, int height, int padding)
+
+{
+	for (int row = 0; row < 2; ++row)
+	{
+		for (int col = 0; col < 3; ++col)
+		{
+			int i = row * 3 + col;
+
+			m_clips[i].x = col * (width + padding) + 10;
+			m_clips[i].y = row * (height + padding) + 10;
+			m_clips[i].w = width;
+			m_clips[i].h = height;
+		}
+	}
 }
