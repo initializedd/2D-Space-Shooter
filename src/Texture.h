cc03159bea2b7bf8cf5d4092080960a08a639916
initializedd <119122691+initializedd@users.noreply.github.com>
2023-04-10 03:58:37 +0100

    Added support to load text as a texture

--- src/Texture.h
+++ src/Texture.h
@@ -1,7 +1,7 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-#include <SDL.h>
+#include <SDL_render.h>
 
 class Texture
 {
@@ -19,12 +19,17 @@ public:
 	bool loadFromPixels(const bool flag, const Uint8 red, const Uint8 green, const Uint8 blue, const Uint8 alpha);
 	bool loadFromFile(const char* path, const bool flag, const Uint8 red = 0xFF, const Uint8 green = 0xFF, const Uint8 blue = 0xFF, const Uint8 alpha = 0xFF);
 
+	bool loadFromRenderedText(const char* text, SDL_Color textColor);
+
 	void render(int x, int y, SDL_Rect* clip = nullptr, int scaleW = 0, int scaleH = 0, double angle = 0.0, SDL_Point* centre = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
 	void resize(int width, int height);
 
+	SDL_Texture* getTexture();
 	int getWidth();
 	int getHeight();
+
+	void setTexture(SDL_Texture* texture);
 
 	void free();
 };
