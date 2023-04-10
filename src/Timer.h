310d25cf6fe5728f724f55c38d1c0f510d404dab
initializedd <bertzhallows@gmail.com>
2023-04-10 03:56:41 +0100

    Initial commit

---
+++ src/Timer.h
@@ -0,0 +1,30 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <SDL_timer.h>
+
+class Timer
+{
+private:
+	Uint32		m_startTicks;
+	Uint32		m_pausedTicks;
+	
+	bool		m_started;
+	bool		m_paused;
+
+public:
+	Timer();
+	~Timer();
+
+	void start();
+	void stop();
+	void pause();
+	void unpause();
+
+	Uint32 getTicks();
+
+	bool isStarted();
+	bool isPaused();
+};
+
+#endif // !TIMER_H
