627926f82ee65f17a2a5d96495e174d9216a3ef8
initializedd <119122691+initializedd@users.noreply.github.com>
2023-04-10 03:56:41 +0100

    Initial commit

---
+++ src/Timer.cpp
@@ -0,0 +1,83 @@
+#include "Timer.h"
+
+Timer::Timer()
+	: m_startTicks{}
+	, m_pausedTicks{}
+	, m_started{}
+	, m_paused{}
+{
+}
+
+Timer::~Timer()
+{
+}
+
+void Timer::start()
+{
+	m_started = true;
+	m_paused = false;
+
+	m_startTicks = SDL_GetTicks();
+	m_pausedTicks = 0;
+}
+
+void Timer::stop()
+{
+	m_started = false;
+	m_paused = false;
+
+	m_startTicks = 0;
+	m_pausedTicks = 0;
+}
+
+void Timer::pause()
+{
+	if (m_started && !m_paused)
+	{
+		m_paused = true;
+
+		m_pausedTicks = SDL_GetTicks() - m_startTicks;
+		m_startTicks = 0;
+	}
+}
+
+void Timer::unpause()
+{
+	if (m_started && m_paused)
+	{
+		m_paused = false;
+
+		m_started = SDL_GetTicks() - m_pausedTicks;
+		m_pausedTicks = 0;
+	}
+}
+
+Uint32 Timer::getTicks()
+{
+	Uint32 time{};
+	if (m_started)
+	{
+		if (m_paused)
+		{
+			time = m_pausedTicks;
+		}
+		else
+		{
+			time = SDL_GetTicks() - m_startTicks;
+		}
+	}
+
+	return time;
+}
+
+
+
+bool Timer::isStarted()
+{
+	return m_started;
+}
+
+bool Timer::isPaused()
+{
+	return m_paused;
+}
