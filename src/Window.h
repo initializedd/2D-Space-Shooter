a127d0bc7bf753c497b25c5052fe8a713c16b5c9
initializedd <119122691+initializedd@users.noreply.github.com>
2023-04-11 03:43:29 +0100

    Moved FPS function here

--- src/Window.h
+++ src/Window.h
@@ -1,17 +1,21 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include "Timer.h"
 #include <SDL_video.h>
 #include <SDL_render.h>
+#include <sstream>
 
 class Window
 {
 private:
-	SDL_Window*		m_window;
-	int				m_width;
-	int				m_height;
+	SDL_Window*				m_window;
+	int						m_width;
+	int						m_height;
 
-	SDL_Renderer*	m_renderer;
+	SDL_Renderer*			m_renderer;
+
+	std::stringstream		m_fpsText;
 
 public:
 	Window();
@@ -20,11 +24,13 @@ public:
 	bool createWindow();
 	bool createRenderer();
 
-	SDL_Window*		getWindow()		const;
-	SDL_Renderer*	getRenderer()	const;
-	
 	void free();
 
+	void calculateFPS(Timer& timer, int countedFrames);
+
+	SDL_Window*			getWindow()		const;
+	SDL_Renderer*		getRenderer()	const;
+	std::stringstream&	getFPS();
 };
 
 #endif // !WINDOW_H
