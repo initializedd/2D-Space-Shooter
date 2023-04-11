38cdaff11104f67f13dbdbcff2dae7c7c21a45c0
initializedd <119122691+initializedd@users.noreply.github.com>
2023-04-11 03:45:58 +0100

    Refactoring collision

--- src/Player.cpp
+++ src/Player.cpp
@@ -8,10 +8,47 @@ Player::Player()
 	, m_height{}
 	, m_pos{}
 	, m_vel{}
+	, m_colliders{}
 	, m_health{}
 	, m_particle{}
 	, m_weapon{}
 {
+	m_colliders.resize(11);
+
+	m_colliders[0].w = 6;
+	m_colliders[0].h = 1;
+
+	m_colliders[1].w = 10;
+	m_colliders[1].h = 1;
+
+	m_colliders[2].w = 14;
+	m_colliders[2].h = 1;
+
+	m_colliders[3].w = 16;
+	m_colliders[3].h = 2;
+
+	m_colliders[4].w = 18;
+	m_colliders[4].h = 2;
+
+	m_colliders[5].w = 20;
+	m_colliders[5].h = 6;
+
+	m_colliders[6].w = 18;
+	m_colliders[6].h = 2;
+
+	m_colliders[7].w = 16;
+	m_colliders[7].h = 2;
+
+	m_colliders[8].w = 14;
+	m_colliders[8].h = 1;
+
+	m_colliders[9].w = 10;
+	m_colliders[9].h = 1;
+
+	m_colliders[10].w = 6;
+	m_colliders[10].h = 1;
+
+	shiftColliders();
 }
 
 Player::~Player()
@@ -81,30 +118,36 @@ void Player::move()
 {
 	// Update X position based on its X velocity
 	m_pos.x += m_vel.x;
+	shiftColliders();
 
 	// Check if outside of left screen boundary
-	if (m_pos.x <= 0)
+	if (m_pos.x <= 0 || checkCollision(m_colliders, gEnemy.getColliders()))
 	{
 		m_pos.x = 0;
+		shiftColliders();
 	}
 	// Check if outside of right screen boundary
-	else if (m_pos.x + m_width >= SCREEN_WIDTH)
+	else if (m_pos.x + m_width >= SCREEN_WIDTH || checkCollision(m_colliders, gEnemy.getColliders()))
 	{
 		m_pos.x = SCREEN_WIDTH - m_width;
+		shiftColliders();
 	}
 
 	// Update Y position based on its Y velocity
 	m_pos.y += m_vel.y;
+	shiftColliders();
 
 	// Check if outside of top screen boundary
-	if (m_pos.y <= 0)
+	if (m_pos.y <= 0 || checkCollision(m_colliders, gEnemy.getColliders()))
 	{
 		m_pos.y = 0;
+		shiftColliders();
 	}
 	// Check if outside of bottom screen boundary
-	else if (m_pos.y + m_height + m_particle.getTexture().getHeight() >= SCREEN_HEIGHT)
+	else if (m_pos.y + m_height + m_particle.getTexture().getHeight() >= SCREEN_HEIGHT || checkCollision(m_colliders, gEnemy.getColliders()))
 	{
 		m_pos.y = SCREEN_HEIGHT - m_height - m_particle.getTexture().getHeight();
+		shiftColliders();
 	}
 }
 
@@ -128,6 +171,69 @@ void Player::animateExhaust(int flameFrames)
 
 	// Right Exhaust
 	m_particle.getTexture().render(gPlayer.getPosX() + (gPlayer.getTexture().getWidth() / 1.41), gPlayer.getPosY() + (gPlayer.getTexture().getHeight() / 1.15), currentClip, m_particle.getTexture().getWidth(), m_particle.getTexture().getHeight(), 180, nullptr, SDL_FLIP_HORIZONTAL);
+}
+
+bool Player::checkCollision(std::vector<SDL_Rect>& a, std::vector<SDL_Rect>& b)
+{
+	//The sides of the rectangles
+	int leftA, leftB;
+	int rightA, rightB;
+	int topA, topB;
+	int bottomA, bottomB;
+
+	//Go through the A boxes
+	for (int Abox = 0; Abox < a.size(); Abox++)
+	{
+		//Calculate the sides of rect A
+		leftA = a[Abox].x;
+		rightA = a[Abox].x + a[Abox].w;
+		topA = a[Abox].y;
+		bottomA = a[Abox].y + a[Abox].h;
+
+		//Go through the B boxes
+		for (int Bbox = 0; Bbox < b.size(); Bbox++)
+		{
+			//Calculate the sides of rect B
+			leftB = b[Bbox].x;
+			rightB = b[Bbox].x + b[Bbox].w;
+			topB = b[Bbox].y;
+			bottomB = b[Bbox].y + b[Bbox].h;
+
+			//If no sides from A are outside of B
+			if (((bottomA <= topB) || (topA >= bottomB) || (rightA <= leftB) || (leftA >= rightB)) == false)
+			{
+				//A collision is detected
+				return true;
+			}
+		}
+	}
+
+	//If neither set of collision boxes touched
+	return false;
+}
+
+void Player::shiftColliders()
+{
+	//The row offset
+	int r = 0;
+
+	//Go through the dot's collision boxes
+	for (int set = 0; set < m_colliders.size(); ++set)
+	{
+		//Center the collision box
+		m_colliders[set].x = m_pos.x + (getTexture().getWidth() - m_colliders[set].w) / 2;
+
+		//Set the collision box at its row offset
+		m_colliders[set].y = m_pos.y + r;
+
+		//Move the row offset down the height of the collision box
+		r += m_colliders[set].h;
+	}
+}
+
+std::vector<SDL_Rect>& Player::getColliders()
+{
+	return m_colliders;
 }
 
 Texture& Player::getTexture()
