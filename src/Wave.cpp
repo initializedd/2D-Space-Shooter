fd87ab974e9a27ffa37489cd50f3f204887381c0
initializedd <bertzhallows@gmail.com>
2023-04-19 04:28:24 +0100

    Refactoring

--- src/Wave.cpp
+++ src/Wave.cpp
@@ -5,13 +5,12 @@ Wave::Wave()
 	: m_enemies{}
 	, m_wave{}
 {
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < 3; ++i)
 	{
-		Enemy enemy{ i * 25, 0 };
-
-		enemy.setTexture(gEnemyTexture);
+		Enemy enemy{ i * 200, 0 };
+		
 		enemy.setCollider();
-
+		
 		m_enemies.push_back(enemy);
 	}
 }
@@ -21,32 +20,6 @@ Wave::~Wave()
 	for (int i = 0; i < m_enemies.size(); ++i)
 	{
 		m_enemies.erase(m_enemies.begin() + i);
-	}
-}
-
-void Wave::render(SDL_Rect* clip, int scaleW, int scaleH, double angle, SDL_Point* centre, SDL_RendererFlip flip)
-{
-	for (int i = 0; i < m_enemies.size(); ++i)
-	{
-		SDL_Rect render_quad{ m_enemies.at(i).getPosX(), m_enemies.at(i).getPosY(), m_enemies.at(i).getTexture().getWidth(), m_enemies.at(i).getTexture().getHeight()};
-
-		if (clip)
-		{
-			render_quad.w = scaleW;
-			render_quad.h = scaleH;
-		}
-
-		SDL_RenderCopyEx(gWindow.getRenderer(), m_enemies.at(i).getTexture().getTexture(), clip, &render_quad, angle, centre, flip);
-	}
-}
-
-void Wave::renderColliders()
-{
-	for (int i = 0; i < m_enemies.size(); ++i)
-	{
-		SDL_SetRenderDrawColor(gWindow.getRenderer(), 0x00, 0xFF, 0x00, 0xFF);
-		SDL_RenderDrawRect(gWindow.getRenderer(), &m_enemies.at(i).getColliders().at(0));
-		SDL_RenderDrawRect(gWindow.getRenderer(), &m_enemies.at(i).getColliders().at(1));
 	}
 }
 
