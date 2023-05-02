9707ce46955c794770284041fd72cd28b25fb99e
initializedd <119122691+initializedd@users.noreply.github.com>
2023-05-02 04:32:33 +0100

    Added copy, move constructors and copy, move assignment operators

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -12,40 +12,81 @@ Texture::Texture()
 {
 }
 
+Texture::Texture(const Texture& other)
+	: m_texture{ other.m_texture }
+	, m_width{ other.m_width }
+	, m_height{ other.m_height }
+	, m_surfacePixels{ other.m_surfacePixels }
+{
+	if (m_texture)
+		++m_texture->count;
+}
+
+Texture::Texture(Texture&& other) noexcept
+	: m_texture{}
+{
+	swap(m_texture, other.m_texture);
+}
+
+Texture& Texture::operator=(const Texture& other)
+{
+	if (this != &other)
+	{
+		if (m_texture && --m_texture->count == 0)
+		{
+			SDL_DestroyTexture(m_texture->texture);
+			delete m_texture;
+			m_texture = nullptr;
+		}
+
+		m_texture = other.m_texture;
+		if (m_texture)
+			++m_texture->count;
+	}
+
+	return *this;
+}
+
+Texture& Texture::operator=(Texture&& other) noexcept
+{
+	swap(m_texture, other.m_texture);
+
+	return *this;
+}
+
 Texture::~Texture()
 {
 	if (m_texture)
 	{
-		SDL_DestroyTexture(m_texture);
-		m_texture = nullptr;
+		if (--m_texture->count == 0)
+		{
+			SDL_DestroyTexture(m_texture->texture);
+			delete m_texture;
+			m_texture = nullptr;
+		}
 	}
 }
 
 bool Texture::loadPixelsFromFile(const char* path)
 {
-	SDL_Surface* loadedSurface{ IMG_Load(path) };
-	if (!loadedSurface)
+	SDL_Surface* surface{ IMG_Load(path) };
+	if (!surface)
 	{
 		printf("Failed to load surface, Error: %s\n", IMG_GetError());
 		return false;
 	}
-	else
-	{
-		m_surfacePixels = SDL_ConvertSurfaceFormat(loadedSurface, SDL_PIXELFORMAT_ABGR8888, 0);
-		if (!m_surfacePixels)
-		{
-			printf("Failed to convert surface format, Error: %s\n", SDL_GetError());
-			return false;
-		}
-		else
-		{
-			m_width = m_surfacePixels->w;
-			m_height = m_surfacePixels->h;
-		}
 
-		SDL_FreeSurface(loadedSurface);
-		loadedSurface = nullptr;
+	m_surfacePixels = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_ABGR8888, 0);
+	if (!m_surfacePixels)
+	{
+		printf("Failed to convert surface format, Error: %s\n", SDL_GetError());
+		return false;
 	}
+
+	m_width = m_surfacePixels->w;
+	m_height = m_surfacePixels->h;
+
+	SDL_FreeSurface(surface);
 
 	return true;
 }
@@ -57,25 +98,27 @@ bool Texture::loadFromPixels(const bool flag, const Uint8 red, const Uint8 green
 		printf("No pixels loaded\n");
 		return false;
 	}
-	else
+
+	SDL_SetColorKey(m_surfacePixels, flag, SDL_MapRGBA(m_surfacePixels->format, red, green, blue, alpha));
+
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(gWindow.getRenderer(), m_surfacePixels);
+	if (!texture)
 	{
-		SDL_SetColorKey(m_surfacePixels, flag, SDL_MapRGBA(m_surfacePixels->format, red, green, blue, alpha));
-
-		m_texture = SDL_CreateTextureFromSurface(gWindow.getRenderer(), m_surfacePixels);
-		if (!m_texture)
-		{
-			printf("Failed to create texture from surface, Error: %s\n", SDL_GetError());
-			return false;
-		}
-		else
-		{
-			m_width = m_surfacePixels->w;
-			m_height = m_surfacePixels->h;
-		}
-
-		SDL_FreeSurface(m_surfacePixels);
-		m_surfacePixels = nullptr;
+		printf("Failed to create texture from surface, Error: %s\n", SDL_GetError());
+		return false;
 	}
+
+	m_texture = new TextureRef(texture);
+	if (!m_texture)
+	{
+		printf("Failed to create texture ref\n");
+		return false;
+	}
+
+	m_width = m_surfacePixels->w;
+	m_height = m_surfacePixels->h;
+
+	SDL_FreeSurface(m_surfacePixels);
 
 	return true;
 }
@@ -87,13 +130,11 @@ bool Texture::loadFromFile(const char* path, const bool flag, const Uint8 red, c
 		printf("Failed to load pixels from file path: %s\n", path);
 		return false;
 	}
-	else
+
+	if (!loadFromPixels(flag, red, green, blue, alpha))
 	{
-		if (!loadFromPixels(flag, red, green, blue, alpha))
-		{
-			printf("Failed to load from pixels\n");
-			return false;
-		}
+		printf("Failed to load from pixels\n");
+		return false;
 	}
 
 	return true;
@@ -110,17 +151,22 @@ bool Texture::loadFromRenderedText(const char* text, TTF_Font* font, SDL_Color t
 		return false;
 	}
 
-	m_texture = SDL_CreateTextureFromSurface(gWindow.getRenderer(), surface);
-	if (!m_texture)
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(gWindow.getRenderer(), surface);
+	if (!texture)
 	{
 		printf("Failed to create texture from surface, Error: %s\n", SDL_GetError());
 		return false;
 	}
-	else
+
+	m_texture = new TextureRef(texture);
+	if (!m_texture)
 	{
-		m_width = surface->w;
-		m_height = surface->h;
+		printf("Failed to create texture ref\n");
+		return false;
 	}
+
+	m_width = surface->w;
+	m_height = surface->h;
 
 	SDL_FreeSurface(surface);
 
@@ -137,7 +183,7 @@ void Texture::render(int x, int y, SDL_Rect* clip, int scaleW, int scaleH, doubl
 		render_quad.h = scaleH;
 	}
 
-	SDL_RenderCopyEx(gWindow.getRenderer(), m_texture, clip, &render_quad, angle, centre, flip);
+	SDL_RenderCopyEx(gWindow.getRenderer(), m_texture->texture, clip, &render_quad, angle, centre, flip);
 }
 
 void Texture::resize(int width, int height)
@@ -146,9 +192,29 @@ void Texture::resize(int width, int height)
 	m_height = height;
 }
 
+void Texture::free()
+{
+	if (m_texture)
+	{
+		if (--m_texture->count == 0)
+		{
+			SDL_DestroyTexture(m_texture->texture);
+			delete m_texture;
+			m_texture = nullptr;
+		}
+	}
+}
+
+void Texture::swap(TextureRef*& first, TextureRef*& second) noexcept
+{
+	TextureRef* temp = first;
+	first = second;
+	second = temp;
+}
+
 SDL_Texture* Texture::getTexture()
 {
-	return m_texture;
+	return m_texture->texture;
 }
 
 int Texture::getWidth()
@@ -158,19 +224,4 @@ int Texture::getWidth()
 int Texture::getHeight()
 {
 	return m_height;
-}
-
-void Texture::setTexture(SDL_Texture* texture)
-{
-	m_texture = texture;
-	texture = nullptr;
-}
-
-void Texture::free()
-{
-	if (m_texture)
-	{
-		SDL_DestroyTexture(m_texture);
-		m_texture = nullptr;
-	}
 }
