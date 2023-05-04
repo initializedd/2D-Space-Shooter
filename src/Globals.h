f12dc1423848cc5b2bc83ab1da15fade6d0e4383
initializedd <119122691+initializedd@users.noreply.github.com>
2023-05-04 03:51:27 +0100

    Reorganised

--- src/Globals.h
+++ src/Globals.h
@@ -9,31 +9,36 @@
 #include "Constants.h"
 #include <SDL_ttf.h>
 
+// Window
 inline Window					gWindow{};
+inline SDL_Surface*				gWindowIconTexture{};
 
+// Background Textures
 inline Texture					gMainMenuTexture{};
+inline Texture					gBackgroundTexture{};
 
-inline Player					gPlayer{SCREEN_WIDTH / 2, SCREEN_HEIGHT};
-inline Wave						gWave{};
+// Text Textures
+inline Texture					gFpsTextTexture{};
 
-inline Texture					gEnemyTexture{};
+// Fonts
+inline TTF_Font*				gFuturaFont{};
 
+// Particle Effects
 inline Particle					gExhaustParticle{};
-inline Sound					gExplosionSound{};
 inline Particle					gExplosionParticle{};
 
-inline Texture					gBackgroundTexture{};
-inline SDL_Surface*				gWindowIconTexture{};
+// Sound Effects
+inline Sound					gExplosionSound{};
+
+// Projectiles
 inline Texture					gProjectileTexture{};
-
-inline SDL_Rect					gRedLaserClip{ 18, 13, 58, 41 };
-inline SDL_Rect					gLeftLaserHitBox{ 26, 1, 4, 31 };
-inline SDL_Rect					gRightLaserHitBox{28, 0, 4, 31};
 inline Sound					gLaserSound{};
+inline SDL_Rect					gRedLaserClip{ 18, 13, 58, 41 };
+inline SDL_Rect					gLeftLaserHitBox{ 26, 7, 4, 27 };
+inline SDL_Rect					gRightLaserHitBox{26, 6, 4, 27};
 
-// Enemy Collision
-inline SDL_Rect					gEnemyHeadCollision{ 0, 69, 170, 38 };
-inline SDL_Rect					gEnemyBodyCollision{ 64, 0, 42, 69 };
+// Player
+inline Player					gPlayer{ SCREEN_WIDTH / 2, SCREEN_HEIGHT };
 
 // Player Collision
 inline SDL_Rect					gHeadCollision{ 56, 23, 38, 55 };
@@ -43,7 +48,12 @@ inline SDL_Rect					gBodyCollision{ 0, 58, 149, 38 };
 inline SDL_Rect					gLowerCollision{ 27, 96, 96, 19 };
 inline SDL_Rect					gExhaustCollision{ 51, 115, 48, 16 };
 
-inline Texture					gFpsTextTexture{};
-inline TTF_Font*				gFuturaFont{};
+// Enemy
+inline Wave						gWave{};
+inline Texture					gEnemyTexture{};
+
+// Enemy Collision
+inline SDL_Rect					gEnemyHeadCollision{ 0, 69, 170, 38 };
+inline SDL_Rect					gEnemyBodyCollision{ 64, 0, 42, 69 };
 
 #endif // !GLOBALS_H
