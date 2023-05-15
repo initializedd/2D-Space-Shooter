54da9d5c5da78853db752fbaa0e7020edfdc6d92
initializedd <119122691+initializedd@users.noreply.github.com>
2023-05-15 03:22:44 +0100

    Updated velocity constants to consider delta time implementation

--- src/Constants.h
+++ src/Constants.h
@@ -6,8 +6,8 @@ const int SCREEN_WIDTH{ 640 };
 const int SCREEN_HEIGHT{ 480 };
 
 // Velocity
-const int PLAYER_VEL{ 6 };
-const int ENEMY_VEL{ 3 };
-const int LASER_VEL{ 10 };
+const int PLAYER_VEL{ 500 };
+const int ENEMY_VEL{ 250 };
+const int LASER_VEL{ 600 };
 
 #endif // !CONSTANTS_H
