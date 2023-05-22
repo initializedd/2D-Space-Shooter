5cea27580f48340afaaba69b114e2917cbe96f00
initializedd <119122691+initializedd@users.noreply.github.com>
2023-05-22 04:31:24 +0100

    Added type alias and changed concept

--- src/Pair.h
+++ src/Pair.h
@@ -4,7 +4,7 @@
 #include <concepts>
 
 template <typename T>
-concept Number = std::is_integral_v<int> || std::is_floating_point_v<double>;
+concept Number = std::is_integral_v<int> || std::is_floating_point_v<float>;
 
 template <Number T>
 struct Pair
@@ -12,5 +12,8 @@ struct Pair
 	T x;
 	T y;
 };
+
+template <Number T>
+using Vector2 = Pair<T>;
 
 #endif // !PAIR_H
