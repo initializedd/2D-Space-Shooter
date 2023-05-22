0e23bd6cd15036d521bc704071fc05db34475aa3
initializedd <119122691+initializedd@users.noreply.github.com>
2023-05-22 04:29:18 +0100

    Ships now have Ship Parts

--- src/Ship.h
+++ src/Ship.h
@@ -1,6 +1,7 @@
 #ifndef SHIP_H
 #define SHIP_H
 
+#include "ShipPart.h"
 #include "Texture.h"
 #include "Weapon.h"
 #include "Particle.h"
@@ -12,16 +13,18 @@
 class Ship
 {
 private:
+	std::vector<ShipPart>	m_parts;
+	int						m_index;
 	Texture					m_texture;
 	SDL_Rect				m_clips[33];
 	//Weapon					m_weapon;
 	Particle				m_exhaustParticle;
-	Pair<int>				m_leftProjectilePos;
-	Pair<int>				m_rightProjectilePos;
 
 public:
 	Ship();
 	~Ship();
+
+	void createShip(int index);
 
 	void setClipsFromSprite(int width, int height, int padding, int elements);
 
