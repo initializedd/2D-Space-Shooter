12c16aca2245d200c63150b80f7de843dff38078
initializedd <bertzhallows@gmail.com>
2023-05-22 04:30:52 +0100

    Refactoring

--- src/Constants.h
+++ src/Constants.h
@@ -5,9 +5,9 @@
 const int SCREEN_WIDTH{ 640 };
 const int SCREEN_HEIGHT{ 480 };
 
-// Velocity
-const int PLAYER_VEL{ 500 };
-const int ENEMY_VEL{ 250 };
-const int LASER_VEL{ 600 };
+// Speeds
+const int PLAYER_SPEED{ 500 };
+const int ENEMY_SPEED{ 250 };
+const int PROJECTILE_SPEED{ 600 };
 
 #endif // !CONSTANTS_H
