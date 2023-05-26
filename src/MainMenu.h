a8911bc93ec5e4e496dfa0274e307ea4d0c41af6
initializedd <119122691+initializedd@users.noreply.github.com>
2023-05-26 23:59:15 +0100

    Refactoring

--- src/MainMenu.h
+++ src/MainMenu.h
@@ -1,5 +1,5 @@
-#ifndef MAINMENU_H
-#define MAINMENU_H
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
 
 #include "Menu.h"
 
@@ -14,4 +14,4 @@ public:
 	void displayMenu(SDL_Event& event, bool& quitGame);
 };
 
-#endif // !MAINMENU_H
+#endif // !MAIN_MENU_H
