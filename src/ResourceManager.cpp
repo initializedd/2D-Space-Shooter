7dd15631d9485a2f7c87924b2056be1ec8549956
initializedd <119122691+initializedd@users.noreply.github.com>
2023-06-15 04:12:06 +0100

    Added more data

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,7 +1,9 @@
 #include "ResourceManager.h"
+#include<flatbuffers/idl.h>
 
 ResourceManager::ResourceManager()
-	: m_renderSystem{}
+	: m_root{}
+	, m_renderSystem{}
 	, m_textureSystem{}
 	, m_soundSystem{}
 	, m_textSystem{}
@@ -9,31 +11,9 @@ ResourceManager::ResourceManager()
 {
 }
 
-bool ResourceManager::init()
-{
-	if (!m_renderSystem.init())
-		return false;
-
-	if (!m_textureSystem.init())
-		return false;
-
-	if (!m_soundSystem.init())
-		return false;
-
-	if (m_textSystem.init())
-		return false;
-
-	return true;
-}
-
-void ResourceManager::free()
+ResourceManager::~ResourceManager()
 {
 	SDL_free(m_dataBuffer);
-
-	m_textSystem.free();
-	m_soundSystem.free();
-	m_textureSystem.free();
-	m_renderSystem.free();
 }
 
 bool ResourceManager::loadDatabase(const std::string& path)
@@ -46,10 +26,65 @@ bool ResourceManager::loadDatabase(const std::string& path)
 		return false;
 	}
 
+	const ResourceDB::Root* root = ResourceDB::GetRoot(data);
+	flatbuffers::Verifier verifier(static_cast<uint8_t*>(data), size);
+	if (!ResourceDB::VerifyRootBuffer(verifier))
+	{
+		printf("Failed to verify root buffer\n");
+		return false;
+	}
+
+	m_root = root;
+
+	for (const auto& texture : *m_root->textures())
+	{
+		m_textureSystem.loadTexture(texture->name()->str(), texture->path()->str());
+	}
+
+	for (const auto& sprite : *m_root->sprites())
+	{
+		m_textureSystem.loadSprite(sprite->name()->str(), sprite->path()->str(), sprite->width(), sprite->height(), sprite->padding(), sprite->elements());
+	}
+
+	for (const auto& sound : *m_root->sounds())
+	{
+		m_soundSystem.loadSound(sound->name()->str(), sound->path()->str());
+	}
+
+	for (const auto& font : *m_root->fonts())
+	{
+		m_textSystem.loadFont(font->name()->str(), font->path()->str(), font->point_size());
+	}
+
+	for (const auto& text : *m_root->texts())
+	{
+		m_textSystem.loadText(text->name()->str(), text->text()->str());
+	}
+
+	for (const auto& textStreams : *m_root->textstreams())
+	{
+		m_textSystem.loadTextStream(textStreams->name()->str());
+	}
+
 	return true;
 }
 
-void ResourceManager::aquire()
+RenderSystem& ResourceManager::getRenderSystem()
 {
+	return m_renderSystem;
+}
 
+TextureSystem& ResourceManager::getTextureSystem()
+{
+	return m_textureSystem;
+}
+
+SoundSystem& ResourceManager::getSoundSystem()
+{
+	return m_soundSystem;
+}
+
+TextSystem& ResourceManager::getTextSystem()
+{
+	return m_textSystem;
 }
