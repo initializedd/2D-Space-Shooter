0f68046de91b021c82cc455a6b42e35ace209187
initializedd <119122691+initializedd@users.noreply.github.com>
2023-06-19 02:59:25 +0100

    Initial commit

---
+++ src/Random.h
@@ -0,0 +1,21 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <chrono>
+#include <random>
+
+inline int getRandomNumber(int min, int max)
+{
+	std::random_device rd{};
+	std::seed_seq ss{ static_cast<std::seed_seq::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()), 
+					  rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() 
+	};
+
+	std::mt19937 mt{ ss };
+
+	std::uniform_int_distribution dist{ min, max };
+	
+	return dist(mt);
+}
+
+#endif // !RANDOM_H
